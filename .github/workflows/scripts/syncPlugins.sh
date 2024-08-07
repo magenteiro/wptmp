ef18c7f5246322434f33949343ac829853d163c0
Ricardo Martins <ricardo@ricardomartins.net.br>
2024-08-07 11:39:50 +1000

    Improvements Deploy script #6

--- .github/workflows/scripts/syncPlugins.sh
+++ .github/workflows/scripts/syncPlugins.sh
@@ -5,8 +5,8 @@ export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-# List installed plugins with version
-wp plugin list --fields=name,version --allow-root > installed_plugins.txt
+# List installed plugins with version and status
+wp plugin list --fields=name,version,status --allow-root > installed_plugins.txt
 
 # Extract names, versions, and statuses of plugins to be synchronized
 grep -oP '"name": "\K[^"]+' .github/workflows/plugins.json > plugins_to_sync_names.txt
@@ -29,9 +29,9 @@ while IFS=, read -r name version status; do
   # Debug: Print the current plugin name, version, and status
   echo "Processing plugin: $name, Version: $version, Status: $status"
 
-  # Check if the plugin is already installed with the same version
-  if grep -q "$name,$version" installed_plugins.txt; then
-    echo "Plugin $name is already installed with version $version. Skipping."
+  # Check if the plugin is already installed with the same version and status
+  if grep -q "$name,$version,$status" installed_plugins.txt; then
+    echo "Plugin $name is already installed with version $version and status $status. Skipping."
     continue
   fi
 
@@ -42,6 +42,7 @@ while IFS=, read -r name version status; do
     echo "Updating plugin: $name"
     wp plugin update "$name" --version="$version" --allow-root
   fi
+
   if [ "$status" = "active" ]; then
     echo "Activating plugin: $name"
     wp plugin activate "$name" --allow-root
@@ -51,9 +52,10 @@ while IFS=, read -r name version status; do
   fi
 done < plugins_to_sync.txt
 
-for installed_plugin in $(cut -d, -f1 installed_plugins.txt); do
-  if ! grep -q "\"name\": \"$installed_plugin\"" .github/workflows/plugins.json; then
-    echo "Deleting plugin: $installed_plugin"
-    wp plugin delete "$installed_plugin" --allow-root
+# Remove plugins not listed in the JSON
+while IFS=, read -r installed_name installed_version installed_status; do
+  if ! grep -q "\"name\": \"$installed_name\"" .github/workflows/plugins.json; then
+    echo "Deleting plugin: $installed_name"
+    wp plugin delete "$installed_name" --allow-root
   fi
-done
+done < installed_plugins.txt
