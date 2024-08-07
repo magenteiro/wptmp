8d9cf5fe0b1b7b697fab880ef191f77a0986f7cb
Ricardo Martins <ricardo@ricardomartins.net.br>
2024-08-07 11:30:31 +1000

    Improvements Deploy script #4

--- .github/workflows/scripts/syncPlugins.sh
+++ .github/workflows/scripts/syncPlugins.sh
@@ -5,9 +5,12 @@ export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-# Sync Plugins
-wp plugin list --field=name --allow-root > installed_plugins.txt
+# List installed plugins with version
+wp plugin list --fields=name,version --allow-root > installed_plugins.txt
+
+# Extract names and versions of plugins to be synchronized
 grep -oP '"name": "\K[^"]+' .github/workflows/plugins.json > plugins_to_sync.txt
+grep -oP '"version": "\K[^"]+' .github/workflows/plugins.json >> plugins_to_sync.txt
 
 # Debug: Print the content of plugins_to_sync.txt
 echo "Plugins to sync:"
@@ -17,17 +20,25 @@ cat plugins_to_sync.txt
 echo "Currently installed plugins:"
 cat installed_plugins.txt
 
-
+# Process each plugin to be synchronized
 while IFS= read -r name; do
-  # Debug: Print the current plugin name
-  echo "Processing plugin: $name"
-  
+  # Read the version from the next line
+  read -r version
+
+  # Debug: Print the current plugin name and version
+  echo "Processing plugin: $name, Version: $version"
+
+  # Check if the plugin is already installed with the same version
+  if grep -q "$name,$version" installed_plugins.txt; then
+    echo "Plugin $name is already installed with version $version. Skipping."
+    continue
+  fi
+
   status=$(grep -A 3 "\"name\": \"$name\"" .github/workflows/plugins.json | grep -oP '"status": "\K[^"]+')
-  version=$(grep -A 3 "\"name\": \"$name\"" .github/workflows/plugins.json | grep -oP '"version": "\K[^"]+')
-  
-  # Debug: Print the status and version
-  echo "Status: $status, Version: $version"
-  
+
+  # Debug: Print the status
+  echo "Status: $status"
+
   if ! grep -q "$name" installed_plugins.txt; then
     echo "Installing plugin: $name"
     wp plugin install "$name" --version="$version" --allow-root
@@ -44,7 +55,7 @@ while IFS= read -r name; do
   fi
 done < plugins_to_sync.txt
 
-for installed_plugin in $(cat installed_plugins.txt); do
+for installed_plugin in $(cut -d, -f1 installed_plugins.txt); do
   if ! grep -q "\"name\": \"$installed_plugin\"" .github/workflows/plugins.json; then
     echo "Deleting plugin: $installed_plugin"
     wp plugin delete "$installed_plugin" --allow-root
